978ee33dcff514d91bc9af3620fe0cb9275988de
shayanxm <shayan.mwp@gmail.com>
2023-03-17 11:14:52 +0330

    asdf

--- src/abstractClass.kt
+++ src/abstractClass.kt
@@ -1,6 +1,6 @@
 /**
  * interface vs abstract class
- *
+ *1. interface can not hold the state but abstract class can.
  */
 fun main() {
 
@@ -15,10 +15,52 @@ fun main() {
 abstract class AnimalZ(){
     //no impelitaion -> class must be abstract then
     abstract val name:String
+
+    protected var energy=0
     //not abstract methods are available
-    fun speak()="Wooof!"
+  open  fun speak()="Wooof!"
+    //return will be Unit be default
+    open fun eat(){
+        energy++
+    }
+
 }
 class Dogz:AnimalZ(){
     override val name="jacki"
+    override fun speak()="Wof wof"
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+    abstract class PersonA(private var name:String){
+        open fun printName(){
+            println(name)
+        }
+    }
+
+    /**in interface each property is abstract
+     * but can have fun with body
+     * not allowed to set valued
+     */
+    interface PersonB{
+     //   abstract val name:String
+        //cannot do that
+        //val age = 12
+
+        fun printName(name:String){
+            println(name)
+        }
+    }
+
+    class Malex:PersonA(name = "shayan"),PersonB{
+
+
+        override fun printName() {
+            super<PersonA>.printName()
+        }
+
+        override fun printName(name: String) {
+            super<PersonB>.printName(name)
+        }
+    }
 
 }
